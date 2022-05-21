4b87a8891d62944852faf5031d46d5f4e09d4bb7
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-05-21 18:05:38 +0300

    Add transformer

---
+++ src/main/java/com/github/stscoundrel/riimut/Transform.java
@@ -0,0 +1,20 @@
+package com.github.stscoundrel.riimut;
+
+import java.util.HashMap;
+
+public class Transform {
+    public static String withDictionary(String content, HashMap<String, String> map) {
+        StringBuilder result = new StringBuilder();
+
+        for (char letter : content.toCharArray()) {
+            String part = String.valueOf(letter).toLowerCase();
+            if (map.containsKey(part)) {
+                result.append(map.get(part));
+            } else {
+                result.append(part);
+            }
+        }
+
+        return result.toString();
+    }
+}

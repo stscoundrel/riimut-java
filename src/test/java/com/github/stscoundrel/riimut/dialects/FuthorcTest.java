f03696024426927b2edf6fda763aa23d6af4df32
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-05-22 20:09:50 +0300

    Add Futhorc dialect

---
+++ src/test/java/com/github/stscoundrel/riimut/dialects/FuthorcTest.java
@@ -0,0 +1,47 @@
+package com.github.stscoundrel.riimut.dialects;
+
+import junit.framework.TestCase;
+
+public class FuthorcTest extends TestCase {
+    public void testTransformsLettersToRunes() {
+        String letters = "aábcdðeéfghiíïʒjklmnŋoóǫpqrstuúvwxyýzåäæœöøþ ";
+        String runes = "ᚪᚪᛒᚳᛞᛞᛖᛖᚠᚷᚻᛁᛇᛇᛇᛡᚳᛚᛗᚾᛝᚩᚩᚩᛈᚳᚱᛋᛏᚢᚢᚹᚹᛉᚣᚣᛉᚩᚫᚫᛟᛟᛟᚦ:";
+
+        Dialect dialect = new Futhorc();
+        String result = dialect.lettersToRunes(letters);
+
+        assertEquals(runes, result);
+    }
+
+    public void testTransformsRunesToLetters() {
+        String runes = "ᚠᚢᚦᚩᚱᚳᚷᚹᚻᚾᛁᛡᛄᛇᛈᛉᛋᚴᛏᛒᛖᛗᛚᛝᛟᛞᚪᚫᚣᛠ:";
+        String letters = "fuþorcgwhnijjïpxsstbemlŋœdaæyea ";
+
+        Dialect dialect = new Futhorc();
+        String result = dialect.runesToLetters(runes);
+
+        assertEquals(letters, result);
+    }
+
+    public void testTransformsTextContentToRunes() {
+        // From 8th century Franks Casket, in late West Saxon.
+        String letters = "fisc.flodu.ahofonferg | enberig |";
+        String runes = "ᚠᛁᛋᚳ.ᚠᛚᚩᛞᚢ.ᚪᚻᚩᚠᚩᚾᚠᛖᚱᚷ:|:ᛖᚾᛒᛖᚱᛁᚷ:|";
+
+        Dialect dialect = new Futhorc();
+        String result = dialect.lettersToRunes(letters);
+
+        assertEquals(runes, result);
+    }
+
+    public void testTransformsRunesToTextContent() {
+        // From 8th century Franks Casket, in late West Saxon.
+        String runes = "ᚠᛁᛋᚳ.ᚠᛚᚩᛞᚢ.ᚪᚻᚩᚠᚩᚾᚠᛖᚱᚷ:|:ᛖᚾᛒᛖᚱᛁᚷ:|";
+        String letters = "fisc.flodu.ahofonferg | enberig |";
+
+        Dialect dialect = new Futhorc();
+        String result = dialect.runesToLetters(runes);
+
+        assertEquals(letters, result);
+    }
+}

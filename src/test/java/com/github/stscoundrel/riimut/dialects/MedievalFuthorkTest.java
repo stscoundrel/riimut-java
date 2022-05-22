2daaa5698086dc41ca826f9153fbc25f32b5d9a8
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-05-22 19:27:00 +0300

    Add Medieval Futhork dialect

---
+++ src/test/java/com/github/stscoundrel/riimut/dialects/MedievalFuthorkTest.java
@@ -0,0 +1,49 @@
+package com.github.stscoundrel.riimut.dialects;
+
+import junit.framework.TestCase;
+
+public class MedievalFuthorkTest extends TestCase {
+    public void testTransformsLettersToRunes() {
+        String letters = "aábcdðeéfghiíjklmnoóǫpqrstuúvwxyýzåäæœöøþ ";
+        String runes = "ᛆᛆᛒᚴᚦᚦᚽᚽᚠᚵᚼᛁᛁᛁᚴᛚᛘᚿᚮᚮᚰᛕᚴᚱᛋᛏᚢᚢᚠᚠᛋᛦᛦᛋᚮᛅᛅᚯᚯᚯᚦ:";
+
+        Dialect dialect = new MedievalFuthork();
+        String result = dialect.lettersToRunes(letters);
+
+        assertEquals(runes, result);
+    }
+
+    public void testTransformsRunesToLetters() {
+        String runes = "ᚠᚢᚦᚮᚱᚴᚼᚿᛁᛆᛌᛋᛐᛏᛒᛘᛚᛦᚯᛅᚰᛕᚽ:";
+        String letters = "fuþorkhniassttbmlyøæǫᴘe ";
+
+        Dialect dialect = new MedievalFuthork();
+        String result = dialect.runesToLetters(runes);
+
+        assertEquals(letters, result);
+    }
+
+    public void testTransformsTextContentToRunes() {
+        // From Lords Prayer in Old Norse.
+        String letters = "Faðer uor som ast i himlüm, halgað warðe þit nama";
+        String runes = "ᚠᛆᚦᚽᚱ:ᚢᚮᚱ:ᛋᚮᛘ:ᛆᛋᛏ:ᛁ:ᚼᛁᛘᛚᚢᛘ,:ᚼᛆᛚᚵᛆᚦ:ᚠᛆᚱᚦᚽ:ᚦᛁᛏ:ᚿᛆᛘᛆ";
+
+        Dialect dialect = new MedievalFuthork();
+        String result = dialect.lettersToRunes(letters);
+
+        assertEquals(runes, result);
+    }
+
+    public void testTransformsRunesToTextContent() {
+        // From Lords Prayer in Old Norse.
+        String runes = "ᚠᛆᚦᚽᚱ:ᚢᚮᚱ:ᛋᚮᛘ:ᛆᛋᛏ:ᛁ:ᚼᛁᛘᛚᚢᛘ:ᚼᛆᛚᚵᛆᚦ:ᚠᛆᚱᚦᚽ:ᚦᛁᛏ:ᚿᛆᛘᛆ";
+
+        // Wont tell apart eth & thorn in mid sentence.
+        String letters = "faþer uor som ast i himlum halgaþ farþe þit nama";
+
+        Dialect dialect = new MedievalFuthork();
+        String result = dialect.runesToLetters(runes);
+
+        assertEquals(letters, result);
+    }
+}

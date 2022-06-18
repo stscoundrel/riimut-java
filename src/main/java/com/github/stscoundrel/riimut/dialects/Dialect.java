0e992f2bdc05a266e2ba3c5cacc9bfc758811817
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2022-06-18 10:37:43 +0300

    Dialect: make a public interface

--- src/main/java/com/github/stscoundrel/riimut/dialects/Dialect.java
+++ src/main/java/com/github/stscoundrel/riimut/dialects/Dialect.java
@@ -1,6 +1,6 @@
 package com.github.stscoundrel.riimut.dialects;
 
-interface Dialect {
+public interface Dialect {
     public String lettersToRunes(String content);
 
     public String runesToLetters(String content);
